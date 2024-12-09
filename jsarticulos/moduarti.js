be019e6e3e5765bc5ec6f4994066ee0425e8fcee
Arturo Miranda <psic.arturomiranda@gmail.com>
2024-12-09 16:14:53 -0600

    202412091614

--- jsarticulos/moduarti.js
+++ jsarticulos/moduarti.js
@@ -1,7 +1,5 @@
+//header
 const cabecera = document.querySelector('header')
-const compartir = document.querySelector('.compartir')
-const botonwhatsart = document.querySelector('.botonwa')
-const footer = document.querySelector('footer')
 
 const barra = document.createElement('nav')
 const lista = document.createElement('ul')
@@ -10,22 +8,20 @@ const li2 = document.createElement('li')
 const li1a = document.createElement('a')
 const li2a = document.createElement('a')
 
-const p1 = document.createElement('p')
-const p2 = document.createElement('p')
-// const ac = document.createElement('a')
-const divaim = document.createElement('span')
-const imgac = document.createElement('img')
-
-const botwa = document.createElement('button')
-const imgwhat = document.createElement('img')
-const awhat = document.createElement('a')
-
-const pfooter = document.createElement('p')
-
 li1a.href = "../inicio.html"
 li1a.textContent = "Inicio"
 li2a.href = "../blog.html"
 li2a.textContent = "Blog"
+
+
+//Compartir
+const compartir = document.querySelector('.compartir')
+
+
+const p1 = document.createElement('p')
+const p2 = document.createElement('p')
+const divaim = document.createElement('span')
+const imgac = document.createElement('img')
 
 p1.textContent = "¿Te gustó este artículo?"
 p2.textContent = "¡Compártelo con tus amigos!"
@@ -33,13 +29,41 @@ imgac.src = "../media/iconos/facebook.png"
 imgac.alt = "Compartir en Facebook"
 imgac.className = "icof"
 
+//sugerencia
+
+const sugerencia = document.getElementById('sugerencia')
+
+const cuadsuger1 = document.createElement('a')
+const h3suger1 = document.createElement('h3')
+const texsuger1 = document.createElement('p')
+const imgsuger1 = document.createElement('img')
+
+h3suger1.textContent = "Ansiedad"
+texsuger1.textContent = "Veremos como quitar la ansiedad"
+imgsuger1.src = "https://images.pexels.com/photos/5542968/pexels-photo-5542968.jpeg"
+cuadsuger1.href = "https://artstrokesmx.github.io/terapiapsicologicacienciayarte/articulos/ansiedad.html"
+/////
+
+
+//boton whats
+const botonwhatsart = document.querySelector('.botonwa')
+
+const botwa = document.createElement('button')
+const imgwhat = document.createElement('img')
+const awhat = document.createElement('a')
+
 imgwhat.src = "../media/iconos/whatsapp.png"
 imgwhat.alt = "Botón Whatsapp"
 imgwhat.loading = "lazy"
 awhat.href = "https://wa.me/525548549863?text=Hola,%20quiero%20%20más%20información%20de%20terapia"
 awhat.textContent = "Agenda una cita con nosotros"
 
+//Footer
+const footer = document.querySelector('footer')
+const pfooter = document.createElement('p')
+
 pfooter.textContent= "© 2024 Terapia Psicológica; Ciencia y Arte"
+
 
 cabecera.appendChild(barra)
 barra.appendChild(lista)
@@ -53,6 +77,10 @@ compartir.appendChild(p1)
 compartir.appendChild(p2)
 compartir.appendChild(divaim)
 
+sugerencia.appendChild(cuadsuger1)
+cuadsuger1.appendChild(h3suger1)
+cuadsuger1.appendChild(texsuger1)
+cuadsuger1.appendChild(imgsuger1)
 
 botonwhatsart.appendChild(botwa)
 botwa.appendChild(imgwhat)
